aa4951f31fbecfdc9910f51a16163f8cd2270100
Bruce <1033684650@qq.com>
2023-06-23 18:41:53 +0800

    modify prettier file

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,9 +1,13 @@
 module.exports = {
-  // 每行最大列，超过换行
-  printWidth: 120,
-  useTab: false,
-  // 缩进
-  tabWidth: 4,
-  // 结尾不用分号
-  semi: false,
+    // 每行最大列，超过换行
+    printWidth: 120,
+    useTab: false,
+    // 缩进
+    tabWidth: 4,
+    // 结尾不用分号
+    semi: false,
+    // 箭头函数里面，如果是一个参数的时候，去掉括号
+    arrowParens: "avoid",
+    // 对象，数组括号与文字间添加空格
+    bracketSpacing: true,
 }
